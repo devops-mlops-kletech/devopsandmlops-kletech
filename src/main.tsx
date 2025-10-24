92ade01338cdeb62c33c9efadf4071f22dc3a390
Rishi <160747822+geeky-rish@users.noreply.github.com>
2025-10-24 21:38:13 +0530

    Fix BrowserRouter rendering syntax

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/devopsandmlops-kletech/"> {}
+  <BrowserRouter basename="/devopsandmlops-kletech/">
     <App />
   </BrowserRouter>
 );
